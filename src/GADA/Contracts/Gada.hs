266f15a50a21c60c193cdc50f1389f4695dba741
tigthor <b.thierry1337@gmail.com>
2022-02-24 01:11:42 +0200

    Making the main module

---
+++ src/GADA/Contracts/Gada.hs
@@ -0,0 +1,27 @@
+{-|
+Module      : Gada
+Description : Main module of the Staking Pool contract.
+Copyright   : P2P Solutions Ltd.
+License     : GPL-3
+Maintainer  : laurynas@adafinance.io
+Stability   : develop
+-}
+
+module GADA.Contracts.Gada
+    ( module GADA.Contracts.Common
+    , module GADA.Contracts.OffChain
+    , module GADA.Contracts.OnChain
+    , module GADA.Contracts.Token
+    , module GADA.Contracts.Types
+    , module GADA.Contracts.Utils
+    , module GADA.Contracts.Trace
+    )
+where
+
+import GADA.Contracts.Common
+import GADA.Contracts.OffChain
+import GADA.Contracts.OnChain
+import GADA.Contracts.Token
+import GADA.Contracts.Types
+import GADA.Contracts.Utils
+import GADA.Contracts.Trace
